65767af89d690d937e7e98490a69a0a95caccf7f
Flupke1979 <42946739+Flupke1979@users.noreply.github.com>
2019-11-07 10:28:43 +0100

    testing

--- scripts/005-Autostart-cncjs.sh
+++ scripts/005-Autostart-cncjs.sh
@@ -4,7 +4,7 @@ sudo npm install -g pm2
 # sudo pm2 startup  # To Start PM2 as root
 pm2 startup  # To start PM2 as pi / current user
   #[PM2] You have to run this command as root. Execute the following command:
-  sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
 
 # Start CNCjs (on port 8000, /w Tinyweb mount point) with PM2
 #pm2 start $(which cncjs) -- --port 8000 -m /tinyweb:/home/pi/tinyweb
